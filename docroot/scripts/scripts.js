adaa5fc358161a58e68cb231c0a200467cbd4989
Dan Gibson <dan@dangibson.me>
2023-03-02 11:51:04 -0500

    Update scripts.js

--- docroot/scripts/scripts.js
+++ docroot/scripts/scripts.js
@@ -1,1 +1,16 @@
 /* scripts file - js and jquery */
+
+
+jQuery(document).ready(function($){
+  $.ajax({
+    type: "GET",
+    url: "https://xandermar.github.io/free-github-hosting-with-clean-urls/sitemap.xml",
+    dataType: "xml",
+    success: function(xml){
+      var xmlSort = $(xml).find("page");
+      
+      // sort based on the cml sort field
+      
+    }
+  });
+}
