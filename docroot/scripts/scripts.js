2c7343f8b28633cc5d2177c7688ce3ef9377f10f
Dan Gibson <dan@dangibson.me>
2023-03-03 12:50:10 -0500

    Update scripts.js

--- docroot/scripts/scripts.js
+++ docroot/scripts/scripts.js
@@ -10,6 +10,42 @@ jQuery(document).ready(function($){
       var xmlSort = $(xml).find("page");
       
       // sort based on the cml sort field
+      xmlSort.sort(function(a,b){
+         a = $(a).find('sort').text();            
+         b = $(b).find('sort').text();            
+         return (a.localCompare(b));
+      });
+      
+      var title, sort, desc, content, path;
+      xmlSort.each(function(){
+        title = $(this).find('title').text();
+        sort = $(this).find('sort').text();
+        desc = $(this).find('desc').text();
+        content = $(this).find('content').text();
+        path = $(this).find('path').text();
+        
+        //builds the 'list-view'
+        $('#list-view).append(`
+          <div class="row">
+            <div class="col-12">
+              <h2><a href="`+path+`">`+title+`</a></h2>
+              <div class="desc">`+desc+`</div>
+            </div>
+          </div>
+          `);
+        
+        //builds the 'detail-view'
+        $('#detail-view).append(`
+          <div class="row">
+            <div class="col-12">
+              <h2>`+title+`</h2>
+              <div class="content">`+content+`</div>
+            </div>
+          </div>          
+          `);
+        
+        
+      });
       
     }
   });
